f51b932e79ba1bfcaf375a200c35831728d63923
Md-ObyduRahaman <sojib.19991018@gmail.com>
2024-02-26 13:10:04 +0600

    swagger has been added

--- src/main/java/com/todo/ToDo/ToDoApplication.java
+++ src/main/java/com/todo/ToDo/ToDoApplication.java
@@ -3,11 +3,21 @@ package com.todo.ToDo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.context.annotation.Bean;
+
+
+import java.util.Collections;
+
 @SpringBootApplication
 public class ToDoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ToDoApplication.class, args);
 	}
+
+
+
+
+
 
 }
